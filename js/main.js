b6a1a1f81465c59a2f6f28d5bf145f38030c4438
bvasilop <bvasilop@gmail.com>
2017-04-25 18:23:43 -0700

    Added smooth scrolling effects

--- js/main.js
+++ js/main.js
@@ -66,6 +66,38 @@ var circle = L.circle([46.927725, -121.493720], 1000, {
     }).addTo(map);
     circle.bindPopup('<b>Crystal Mountain Ski Resort</b>');
 
+    /* smooth scrolling effects */
+
+
+ $(document).ready(function(){
+   // Add smooth scrolling to all links
+   $("a").on('click', function(event) {
+
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+       // Prevent default anchor click behavior
+       event.preventDefault();
+
+       // Store hash
+       var hash = this.hash;
+
+       // Using jQuery's animate() method to add smooth page scroll
+       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800, function(){
+
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+ });
+
+
+
+
+
 
 
 });
