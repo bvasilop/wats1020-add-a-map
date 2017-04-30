2c2e74328a0fd7b0118d36b4862ac095b2572a11
bvasilop <bvasilop@gmail.com>
2017-04-30 14:29:49 -0700

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -140,6 +140,7 @@ marker.on('mouseleave', function (e) {
        var hash = this.hash;
        // Using jQuery's animate() method to add smooth page scroll
        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       // -80 refers to scroll offset amount to compensate for fixed nav
        $('html, body').animate({
          scrollTop: $(hash).offset().top -80
        }, 800, function(){
